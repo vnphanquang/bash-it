96cd7c25fec736170d1e23be50d540e8af19f407
Quang Phan <quangpn@dgroup.co>
2020-08-13 13:11:52 +0700

    style(clock): turn on predefined clock

--- themes/farius/farius.theme.bash
+++ themes/farius/farius.theme.bash
@@ -33,7 +33,7 @@ function battery_char {
 
 __farius_clock() {
   if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
-    printf "$(clock_char) "
+    printf "$(clock_char)"
   fi
   printf "$(clock_prompt)"
 }
@@ -69,12 +69,12 @@ function prompt_command() {
     git_branch_emoji
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
     # PS1="\n$(battery_char) $(__farius_clock)${yellow}$(ruby_version_prompt) $(user_host_prompt) ${reset_color}in ${green}\w\n$(scm_prompt_char_info) ${green}→${reset_color} "
-    PS1="$(user_host_prompt) ${reset_color}in ${green}\w \D{%Y.%m.%d-%H:%M:%S}\n$(scm_prompt_char_info) ${green}→${reset_color} "
+    PS1="$(__farius_clock) $(user_host_prompt) ${reset_color}in ${green}\w\n$(scm_prompt_char_info) ${green}→${reset_color} "
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
-THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
+THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$yellow"}
 THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
-THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y.%m.%d %H:%M:%S"}
 
 safe_append_prompt_command prompt_command
